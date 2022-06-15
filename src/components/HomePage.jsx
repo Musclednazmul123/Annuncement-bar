a8d918806e951d43cd2d19d00551596d7da2dc94
Nazmul Hawlader <97741145+Musclednazmul123@users.noreply.github.com>
2022-06-15 17:46:17 +0600

    initial commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,52 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Heading,
+} from "@shopify/polaris";
+
+import trophyImgUrl from "../assets/home-trophy.png";
+
+import { ProductsCard } from "./ProductsCard";
+
+export function HomePage() {
+  return (
+    <Page fullWidth>
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Stack
+              wrap={false}
+              spacing="extraTight"
+              distribution="trailing"
+              alignment="center"
+            >
+              <Stack.Item fill>
+                <TextContainer spacing="loose">
+                  <Heading>Welcome to my annuncement Apps</Heading>
+                  
+                </TextContainer>
+              </Stack.Item>
+              <Stack.Item>
+                {/* <div style={{ padding: "0 20px" }}>
+                  <Image
+                    source={trophyImgUrl}
+                    alt="Nice work on building a Shopify app"
+                    width={120}
+                  />
+                </div> */}
+              </Stack.Item>
+            </Stack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section secondary>
+          {/* <ProductsCard /> */}
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
